df0ecf6d23d984cd14d7ba346e59391a6071e270
Nathan <93925029+Nath9666@users.noreply.github.com>
2023-06-08 12:24:01 +0200

    afficher dans le cli

--- main.py
+++ main.py
@@ -28,13 +28,20 @@ class Formes:
         self.y = 0
 
     def rotation(self, pas=1):
+        """Effectue une rotation de la forme
+        :param pas: nombre de rotation à effectuer"""
         self.forme = np.rot90(self.forme, pas)
 
     def move(self, x, y):
+        """Déplace la forme
+        :param x: déplacement en x
+        :param y: déplacement en y"""
         self.x += x
         self.y += y
 
     def __str__(self) -> str:
+        """
+        Affiche la forme et sa position"""
         return (
             "Forme : "
             + str(self.forme)
