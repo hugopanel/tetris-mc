7c3305bd89fa4161d030ae2c10d7c1a12a30efa3
Hugo PANEL <hugopanel@gmail.com>
2023-06-14 09:36:48 +0200

    Reformat + Ajout case pour afficher la forme suivante

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ class Tileset:
         self.file = file
         self.size = size
         self.image = pygame.image.load(file)
-        # self.image = pygame.transform.scale(self.image, (self.image.get_size[0] * 3, self.image.get_size[1] * 3))
         self.rect = self.image.get_rect()
         self.tiles = []
         self.load()
@@ -37,35 +36,114 @@ screen.blit(tileset.tiles[4], tileset.tiles[4].get_rect())
 
 import numpy as np
 interface = np.full((32, 30), 12, dtype=int)
-# interface = np.full((32, 30), 1, dtype=int)
+# interface = np.full((32, 30), 0, dtype=int)
 
-interface[3][1] = 17
-interface[4][1] = 9
-interface[5][1] = 9
-interface[6][1] = 9
-interface[7][1] = 16
-interface[3][2] = 10
-interface[3][3] = 10
-interface[3][4] = 10
-interface[3][5] = 10
-interface[3][6] = 10
+# Carré grille de jeu
+# Coin haut gauche
+interface[3][6] = 17
+# Ligne dessus
+interface[4][6] = 9
+interface[5][6] = 9
+interface[6][6] = 9
+interface[7][6] = 9
+interface[8][6] = 9
+interface[9][6] = 9
+interface[10][6] = 9
+interface[11][6] = 9
+interface[12][6] = 9
+interface[13][6] = 9
+# Coin haut droite
+interface[14][6] = 16
+# Ligne gauche
 interface[3][7] = 10
-interface[7][2] = 8
-interface[7][3] = 8
-interface[7][4] = 8
-interface[7][5] = 8
-interface[7][6] = 8
-interface[7][7] = 8
-interface[3][8] = 15
-interface[4][8] = 7
-interface[5][8] = 7
-interface[6][8] = 7
-interface[7][8] = 14
+interface[3][8] = 10
+interface[3][9] = 10
+interface[3][10] = 10
+interface[3][11] = 10
+interface[3][12] = 10
+interface[3][13] = 10
+interface[3][14] = 10
+interface[3][15] = 10
+interface[3][16] = 10
+interface[3][17] = 10
+interface[3][18] = 10
+interface[3][19] = 10
+interface[3][20] = 10
+interface[3][21] = 10
+interface[3][22] = 10
+interface[3][23] = 10
+interface[3][24] = 10
+interface[3][25] = 10
+interface[3][26] = 10
+# Ligne droite
+interface[14][7] = 8
+interface[14][8] = 8
+interface[14][9] = 8
+interface[14][10] = 8
+interface[14][11] = 8
+interface[14][12] = 8
+interface[14][13] = 8
+interface[14][14] = 8
+interface[14][15] = 8
+interface[14][16] = 8
+interface[14][17] = 8
+interface[14][18] = 8
+interface[14][19] = 8
+interface[14][20] = 8
+interface[14][21] = 8
+interface[14][22] = 8
+interface[14][23] = 8
+interface[14][24] = 8
+interface[14][25] = 8
+interface[14][26] = 8
+# Coin bas gauche
+interface[3][27] = 15
+# Ligne bas
+interface[4][27] = 7
+interface[5][27] = 7
+interface[6][27] = 7
+interface[7][27] = 7
+interface[8][27] = 7
+interface[9][27] = 7
+interface[10][27] = 7
+interface[11][27] = 7
+interface[12][27] = 7
+interface[13][27] = 7
+# Coin bas droit
+interface[14][27] = 14
+
+# Carré forme suivante
+# Coin haut gauche
+interface[18][6] = 17
+# Ligne haut
+interface[19][6] = 9
+interface[20][6] = 9
+interface[21][6] = 9
+interface[22][6] = 9
+# Coin haut droit
+interface[23][6] = 16
+# Ligne gauche
+interface[18][7] = 10
+interface[18][8] = 10
+interface[18][9] = 10
+interface[18][10] = 10
+# Ligne droite
+interface[23][7] = 8
+interface[23][8] = 8
+interface[23][9] = 8
+interface[23][10] = 8
+# Coin bas gauche
+interface[18][11] = 15
+# Ligne bas
+interface[19][11] = 7
+interface[20][11] = 7
+interface[21][11] = 7
+interface[22][11] = 7
+# Coin bas droit
+interface[23][11] = 14
 
 for row in range(0, len(interface)):
-    print(row)
     for col in range(0, len(interface[row])):
-        print(col)
         screen.blit(tileset.tiles[interface[row][col]], (row*8, col*8), tileset.tiles[interface[row][col]].get_rect())
 
 window.blit(pygame.transform.scale(screen, window.get_rect().size), (0, 0))
